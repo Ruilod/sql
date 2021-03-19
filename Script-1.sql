fd03c4efb8bb5d788674fa947443f52a009c1257
Ruilod <Rafaelh2000@yandex.ru>
2021-03-19 22:38:56 +0300

    added the solve for 3rd quest

--- Script-1.sql
+++ Script-1.sql
@@ -8,3 +8,12 @@ SELECT IF((SELECT COUNT(*) FROM likes WHERE user_id IN (SELECT user_id FROM prof
 SELECT COUNT(*) FROM likes WHERE target_id IN 
 (SELECT id FROM media WHERE user_id IN(SELECT * FROM (SELECT user_id FROM profiles ORDER BY birthday DESC LIMIT 10) subtable));
 
+-- Выборка наименее активных пользователей 
+SELECT 
+  CONCAT(first_name, ' ', last_name) AS user, 
+	(SELECT COUNT(*) FROM likes WHERE likes.user_id = profiles.user_id) + 
+	(SELECT COUNT(*) FROM media WHERE media.user_id = profiles.user_id) + 
+	(SELECT COUNT(*) FROM messages WHERE messages.from_user_id = profiles.user_id) AS activity
+	  FROM profiles 
+	  ORDER BY activity
+	  LIMIT 10;	
